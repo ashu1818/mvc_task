fc9b80782d5c34df7b88f6c31cebb87ece0b69d1
ashu1818 <apandherpatte@gmail.com>
2019-09-06 19:01:36 +0530

    Add project files.

---
+++ task_mvc/Controllers/ShowIController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task_mvc.Controllers
+{
+    public class ShowIController : Controller
+    {
+        // GET: ShowI
+        public ActionResult GetImage()
+
+        {
+            string imgPath = Server.MapPath("~/Content/Images/im.jpg");
+            byte[] bytedata = System.IO.File.ReadAllBytes(imgPath);
+            string imgBase64Data = Convert.ToBase64String(bytedata);
+            string imgDataURL = string.Format("data:image/jpg;base64,{0}", imgBase64Data);
+            ViewBag.ImageData = imgDataURL;
+            return View();
+        }
+    }
+}
